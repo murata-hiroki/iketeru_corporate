c74af4bbcfd8d8ddcdd98a232e49bf9d9be0b82f
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-07 19:31:55 +0900

    背景画像をStaticImageに

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,19 +1,29 @@
+import { StaticImage } from 'gatsby-plugin-image'
 import * as styles from './styles.module.scss'
 import Button from '../Button'
 
 const Hero = () => {
   return (
     <div className={styles.outer}>
-      <h1 className={styles.title}>
-        まだWordPressを<br />使っている方必見！！
-      </h1>
-      <p className={styles.catchcopy}>
-        あなたのサイト、<br />
-        もっと高速に、<br />
-        もっとセキュリティを強固に。
-      </p>
-      <div className={styles.btnBox}>
-        <Button>無料お見積もりはこちら</Button>
+      <StaticImage src='../../images/hero-bg.png' className={styles.bg} />
+      <StaticImage src='../../images/hero-left.png' className={styles.figureLeft} />
+      <StaticImage
+        src='../../images/hero-right.png'
+        className={styles.figureRight}
+        placeholder='tracedSVG'
+      />
+      <div className={styles.inner}>
+        <h1 className={styles.title}>
+          まだWordPressを<br />使っている方必見！！
+        </h1>
+        <p className={styles.catchcopy}>
+          あなたのサイト、<br />
+          もっと高速に、<br />
+          もっとセキュリティを強固に。
+        </p>
+        <div className={styles.btnBox}>
+          <Button>無料お見積もりはこちら</Button>
+        </div>
       </div>
     </div>
   )
