d01c359042db27a9fdbab50e3d821735b45c661f
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-08 19:50:43 +0900

    h2 to h3

--- src/components/Heading3/index.jsx
+++ src/components/Heading3/index.jsx
@@ -1,7 +1,7 @@
 import * as styles from './styles.module.scss'
 
 const Heading3 = ({ children }) => {
-  return <h2 className={styles.h2}>{children}</h2>
+  return <h3 className={styles.h2}>{children}</h3>
 }
 
 export default Heading3
