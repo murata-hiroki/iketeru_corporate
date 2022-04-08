b9ec5177a4ff79ac54422a1f5c54f740392ed4ef
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-08 22:03:03 +0900

    seo title

---
+++ src/components/Seo/index.jsx
@@ -0,0 +1,12 @@
+import Heading3 from '../Heading3'
+import SubSection from '../SubSection'
+
+const Seo = () => {
+  return (
+    <SubSection>
+      <Heading3>サイトの高速化はSEO対策にも有効</Heading3>
+    </SubSection>
+  )
+}
+
+export default Seo
