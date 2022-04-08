d0ff17819019e0867020145fb464acfd90274bf7
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-08 22:11:14 +0900

    seo sentence

--- src/components/Seo/index.jsx
+++ src/components/Seo/index.jsx
@@ -1,10 +1,17 @@
 import Heading3 from '../Heading3'
 import SubSection from '../SubSection'
+import Sentence from '../Sentence'
 
 const Seo = () => {
   return (
     <SubSection>
       <Heading3>サイトの高速化はSEO対策にも有効</Heading3>
+      <Sentence>
+        <p>
+          Googleは2021年から、ウェブサイトの表示スピードを、検索ランキングの指標の1つとして導入しました。したがって、サイトを高速化することにより、検索順位に良い影響を与えることが期待できます。<br />
+          Gatsbyへの移行で、サイトを高速化しませんか？
+        </p>
+      </Sentence>
     </SubSection>
   )
 }
