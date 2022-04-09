24215f2ea6b8648dba887cf07a857910e325611f
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-09 11:07:03 +0900

    migration logo

--- src/components/Migration/index.jsx
+++ src/components/Migration/index.jsx
@@ -1,10 +1,18 @@
+import { StaticImage } from 'gatsby-plugin-image'
 import Heading3 from '../Heading3'
 import SubSection from '../SubSection'
+import ArrowRight from '../../inline-svg/arrow-right.svg'
+import gatsbyLogo from '../../images/gatsby.svg'
 
 const Migration = () => {
   return (
     <SubSection>
       <Heading3>Gatsbyへの移行は、イケテルシステムにお任せを！</Heading3>
+      <div>
+        <StaticImage src='../../images/wordpress.png' alt='WordPress' />
+        <ArrowRight />
+        <img src={gatsbyLogo} alt='Gatsby' />
+      </div>
     </SubSection>
   )
 }
