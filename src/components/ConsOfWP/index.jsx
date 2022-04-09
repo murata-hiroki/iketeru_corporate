d0301c43c7c7c0381dc4546ae07a61dfb64dafdb
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-09 21:54:29 +0900

    問題点

--- src/components/ConsOfWP/index.jsx
+++ src/components/ConsOfWP/index.jsx
@@ -1,10 +1,13 @@
+import Cards from '../Cards'
 import Heading3 from '../Heading3'
 import SubSection from '../SubSection'
+import { cardListData } from './cardListData'
 
 const ConsOfWP = () => {
   return (
     <SubSection>
       <Heading3>WordPressの問題点</Heading3>
+      <Cards cards={cardListData} />
     </SubSection>
   )
 }
