e42d77720a203617bbc1662a24aad3f88e52d50d
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-09 13:19:13 +0900

    estimate form

---
+++ src/components/EstimateForm/index.jsx
@@ -0,0 +1,11 @@
+import Button from '../Button'
+
+const EstimateForm = () => {
+  return (
+    <form action=''>
+      <Button>送信する</Button>
+    </form>
+  )
+}
+
+export default EstimateForm
--- src/components/EstimateSection/index.jsx
+++ src/components/EstimateSection/index.jsx
@@ -1,5 +1,6 @@
 import Container from '../Container'
 import Cta from '../Cta'
+import EstimateForm from '../EstimateForm'
 import Heading2 from '../Heading2'
 import Section from '../Section'
 
@@ -9,6 +10,7 @@ const EstimateSection = () => {
       <Container>
         <Heading2>無料お見積りはこちらから</Heading2>
         <Cta hideBtn />
+        <EstimateForm />
       </Container>
     </Section>
   )
