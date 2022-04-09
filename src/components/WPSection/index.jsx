157c9ca925d3ebc79ac60784d9afe875458877fa
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-09 20:33:52 +0900

    wp lead text

--- src/components/WPSection/index.jsx
+++ src/components/WPSection/index.jsx
@@ -1,6 +1,7 @@
 import Container from '../Container'
 import Heading2 from '../Heading2'
 import Section from '../Section'
+import Sentence from '../Sentence'
 import ToFootnote from '../ToFootnote'
 
 const WPSection = () => {
@@ -10,6 +11,13 @@ const WPSection = () => {
         <Heading2>
           WordPress<ToFootnote />の時代は終わった。
         </Heading2>
+        <Sentence>
+          <p>
+            WordPressは、ホームページを簡単に作成することができるすばらしいシステムです。<br />
+            ここまでWebが普及したのは、WordPressの貢献によるところが大きいでしょう。<br />
+            しかし、近年、WordPressのセキュリティに対する不安や、スピード面での課題が問題視されるようになってきました。
+          </p>
+        </Sentence>
       </Container>
     </Section>
   )
