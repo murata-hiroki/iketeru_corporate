cc765f4772032ed21acf55bdf2710e9f3ac5030e
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-09 09:29:31 +0900

    比較表アイコン

--- src/components/CompareTable/index.jsx
+++ src/components/CompareTable/index.jsx
@@ -1,3 +1,7 @@
+import DoubleCircle from '../../inline-svg/double-circle.svg'
+import Circle from '../../inline-svg/circle.svg'
+import Triangle from '../../inline-svg/triangle.svg'
+
 const CompareTable = () => {
   return (
     <table>
@@ -16,23 +20,47 @@ const CompareTable = () => {
       <tbody>
         <tr>
           <td>表示スピード</td>
-          <td>非常に速い</td>
-          <td>遅くなりやすい</td>
+          <td>
+            <DoubleCircle />
+            非常に速い
+          </td>
+          <td>
+            <Triangle />
+            遅くなりやすい
+          </td>
         </tr>
         <tr>
           <td>セキュリティ</td>
-          <td>セキュリティリスクが低い</td>
-          <td>セキュリティリスクあり</td>
+          <td>
+            <Circle />
+            セキュリティリスクが低い
+          </td>
+          <td>
+            <Triangle />
+            セキュリティリスクあり
+          </td>
         </tr>
         <tr>
           <td>管理コスト</td>
-          <td>ソフトウェアのアップデートをチェックする頻度を減らせる</td>
-          <td>管理が煩雑でソフトウェアのアップデートを常にチェックし続ける必要あり</td>
+          <td>
+            <Circle />
+            ソフトウェアのアップデートをチェックする頻度を減らせる
+          </td>
+          <td>
+            <Triangle />
+            管理が煩雑でソフトウェアのアップデートを常にチェックし続ける必要あり
+          </td>
         </tr>
         <tr>
           <td>サーバーコスト</td>
-          <td>大抵の場合、無料</td>
-          <td>月額1000円程度が相場</td>
+          <td>
+            <DoubleCircle />
+            大抵の場合、無料
+          </td>
+          <td>
+            <Triangle />
+            月額1000円程度が相場
+          </td>
         </tr>
       </tbody>
     </table>
