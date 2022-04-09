3bd90a9055bcc0ef28420cc928e4e73a11be7540
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-09 11:50:36 +0900

    faqlist

--- src/components/Faq/index.jsx
+++ src/components/Faq/index.jsx
@@ -1,10 +1,22 @@
+import Container from '../Container'
 import Heading2 from '../Heading2'
 import Section from '../Section'
+import { faqList } from './faqList'
 
 const Faq = () => {
   return (
     <Section>
-      <Heading2>よくあるご質問</Heading2>
+      <Container>
+        <Heading2>よくあるご質問</Heading2>
+        <div>
+          {faqList.map(faqItem =>
+            <section key={faqItem.q}>
+              <h3>{faqItem.q}</h3>
+              <p>{faqItem.a}</p>
+            </section>
+          )}
+        </div>
+      </Container>
     </Section>
   )
 }
