c794660246205fc7076eb127f5148f29530f28d8
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-09 11:41:04 +0900

    faq title

---
+++ src/components/Faq/index.jsx
@@ -0,0 +1,12 @@
+import Heading2 from '../Heading2'
+import Section from '../Section'
+
+const Faq = () => {
+  return (
+    <Section>
+      <Heading2>よくあるご質問</Heading2>
+    </Section>
+  )
+}
+
+export default Faq
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,14 @@
 import '../scss/globals.scss'
 import Hero from '../components/Hero'
 import NewTechnologySection from '../components/NewTechnologySection'
+import Faq from '../components/Faq'
 
 const Page = () => {
   return (
     <>
       <Hero />
       <NewTechnologySection />
+      <Faq />
     </>
   )
 }
