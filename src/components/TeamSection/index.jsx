c4068f659015d6ff5004c97b9d5ee514f417e4f6
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-10 12:15:01 +0900

    team heading and sentence

---
+++ src/components/TeamSection/index.jsx
@@ -0,0 +1,23 @@
+import Container from '../Container'
+import Heading2 from '../Heading2'
+import Section from '../Section'
+import Sentence from '../Sentence'
+
+const TeamSection = () => {
+  return (
+    <Section>
+      <Container>
+        <Heading2>制作チームについて</Heading2>
+        <Sentence>
+          <p>
+            イケテルシステムは、Web開発のプロフェッショナルが集うチームです。<br />
+            代表の小野は、IT企業の役員として、CTO（最高技術責任者）を務めた経験があります。<br />
+            経験豊富なリーダーが率いるチームですので、安心してお任せいただけます！
+          </p>
+        </Sentence>
+      </Container>
+    </Section>
+  )
+}
+
+export default TeamSection
