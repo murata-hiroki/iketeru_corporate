5b21aae803c02b4ea1dcb168520c3445417b6535
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-11 08:34:26 +0900

    footnote

--- src/components/Footnote/index.jsx
+++ src/components/Footnote/index.jsx
@@ -1,17 +1,23 @@
+import * as styles from './styles.module.scss'
+
 const wpUrl = 'https://wordpressfoundation.org/'
 const gatsbyUrl = 'https://www.gatsbyjs.com/'
 
 const Footnote = () => {
   return (
-    <ul>
+    <ul className={styles.footnote}>
       <li>
         「WordPress」は、
-        <a href={wpUrl} target='_blank' rel='noopener noreferrer'>WordPress Foundation</a>
+        <a href={wpUrl} target='_blank' rel='noopener noreferrer'>
+          WordPress Foundation
+        </a>
         の商標です。
       </li>
       <li>
         「Gatsby」は、
-        <a href={gatsbyUrl} target='_blank' rel='noopener noreferrer'>Gatsby, Inc.</a>
+        <a href={gatsbyUrl} target='_blank' rel='noopener noreferrer'>
+          Gatsby, Inc.
+        </a>
         のプロダクトです。
       </li>
     </ul>
