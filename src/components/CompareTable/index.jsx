20df725147260b3d63eecb8537087b0a962be10d
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-12 22:26:37 +0900

    compare table style

--- src/components/CompareTable/index.jsx
+++ src/components/CompareTable/index.jsx
@@ -1,69 +1,80 @@
+import * as styles from './styles.module.scss'
 import DoubleCircle from '../../inline-svg/double-circle.svg'
 import Circle from '../../inline-svg/circle.svg'
 import Triangle from '../../inline-svg/triangle.svg'
 
 const CompareTable = () => {
   return (
-    <table>
-      <colgroup>
-        <col />
-        <col />
-        <col />
-      </colgroup>
-      <thead>
-        <tr>
-          <td />
-          <th>Gatsby</th>
-          <th>WordPress</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>表示スピード</td>
-          <td>
-            <DoubleCircle />
-            非常に速い
-          </td>
-          <td>
-            <Triangle />
-            遅くなりやすい
-          </td>
-        </tr>
-        <tr>
-          <td>セキュリティ</td>
-          <td>
-            <Circle />
-            セキュリティリスクが低い
-          </td>
-          <td>
-            <Triangle />
-            セキュリティリスクあり
-          </td>
-        </tr>
-        <tr>
-          <td>管理コスト</td>
-          <td>
-            <Circle />
-            ソフトウェアのアップデートをチェックする頻度を減らせる
-          </td>
-          <td>
-            <Triangle />
-            管理が煩雑でソフトウェアのアップデートを常にチェックし続ける必要あり
-          </td>
-        </tr>
-        <tr>
-          <td>サーバーコスト</td>
-          <td>
-            <DoubleCircle />
-            大抵の場合、無料
-          </td>
-          <td>
-            <Triangle />
-            月額1000円程度が相場
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div className={styles.wrapper}>
+      <table className={styles.table}>
+        <colgroup>
+          <col />
+          <col className={styles.gatsbyCol} />
+          <col />
+        </colgroup>
+        <thead>
+          <tr>
+            <td className={styles.emptyHead} />
+            <th className={styles.gatsbyHead}>Gatsby</th>
+            <th className={styles.wpHead}>WordPress</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className={styles.bodyRow}>
+            <td className={styles.itemHead}>
+              表示スピード
+            </td>
+            <td className={styles.desc}>
+              <div><DoubleCircle /></div>
+              非常に速い
+            </td>
+            <td className={styles.desc}>
+              <div><Triangle /></div>
+              遅くなりやすい
+            </td>
+          </tr>
+          <tr className={styles.bodyRow}>
+            <td className={styles.itemHead}>
+              セキュリティ
+            </td>
+            <td className={styles.desc}>
+              <div><Circle /></div>
+              セキュリティリスクが低い
+            </td>
+            <td className={styles.desc}>
+              <div><Triangle /></div>
+              セキュリティリスクあり
+            </td>
+          </tr>
+          <tr className={styles.bodyRow}>
+            <td className={styles.itemHead}>
+              管理コスト
+            </td>
+            <td className={styles.desc}>
+              <div><Circle /></div>
+              ソフトウェアのアップデートをチェックする頻度を減らせる
+            </td>
+            <td className={styles.desc}>
+              <div><Triangle /></div>
+              管理が煩雑で、ソフトウェアのアップデートを常にチェックし続ける必要あり
+            </td>
+          </tr>
+          <tr className={styles.bodyRow}>
+            <td className={styles.itemHead}>
+              サーバーコスト
+            </td>
+            <td className={styles.desc}>
+              <div><DoubleCircle /></div>
+              大抵の場合、無料
+            </td>
+            <td className={styles.desc}>
+              <div><Triangle /></div>
+              月額1000円程度が相場
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
