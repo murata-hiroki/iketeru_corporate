9aed665b4bd43ead4a1a65021ba953db56a16dd1
Yuto Ono <stellarwindmaster@gmail.com>
2022-04-15 13:37:00 +0900

    responsive

--- src/components/Large/index.jsx
+++ src/components/Large/index.jsx
@@ -1,5 +1,7 @@
+import * as styles from './styles.module.scss'
+
 const Large = ({ children }) => {
-  return <span>{children}</span>
+  return <span className={styles.lg}>{children}</span>
 }
 
 export default Large
