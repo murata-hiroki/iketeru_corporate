be84f34abc3903900f9e2a149513864cd6584cad
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-22 11:25:30 +0900

    Button emotion 移行

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,4 +1,5 @@
-import * as styles from './styles.module.scss'
+import { css } from '@emotion/react'
+import { colors } from '../../utilities/styleUtilities'
 
 const Button = ({
   children,
@@ -9,13 +10,13 @@ const Button = ({
 }) => {
   if (type === 'submit') {
     return (
-      <button type='submit' className={styles.btn} disabled={disabled}>
+      <button type='submit' css={styles.btn} disabled={disabled}>
         {children}
       </button>
     )
   } else {
     return (
-      <a href={href} className={styles.btn} onClick={onClick}>
+      <a href={href} css={styles.btn} onClick={onClick}>
         {children}
       </a>
     )
@@ -23,3 +24,35 @@ const Button = ({
 }
 
 export default Button
+
+const styles = {
+  btn: css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    width: 440px;
+    height: 86px;
+    border-radius: 4px;
+    border-style: none;
+    background-color: ${colors.purple};
+    font-size: 24px;
+    color: ${colors.white};
+    text-decoration: none;
+    cursor: pointer;
+
+    @include sm {
+      width: 100%;
+      height: 60px;
+      font-size: 18px;
+    }
+
+    &:disabled {
+      opacity: 0.4;
+      cursor: default;
+    }
+
+    &:hover {
+      opacity: 0.8;
+    }
+  `
+}
