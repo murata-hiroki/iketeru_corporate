4cc129a28a03bc218ad331cb538469739b1d2600
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-22 11:34:48 +0900

    Card emotion移行

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,42 @@
-import * as styles from './styles.module.scss'
+import { css } from '@emotion/react'
+import { mq } from '../../utilities/styleUtilities'
 
 const Card = ({ icon, title, children }) => {
   return (
     <section>
-      <div className={styles.icon}>{icon}</div>
-      <h4 className={styles.title}>{title}</h4>
-      <div className={styles.desc}>{children}</div>
+      <div css={styles.icon}>{icon}</div>
+      <h4 css={styles.title}>{title}</h4>
+      <div css={styles.desc}>{children}</div>
     </section>
   )
 }
 
 export default Card
+
+const styles = {
+  icon: css`
+    margin-bottom: 20px;
+    text-align: center;
+    ${mq.sm} {
+      margin-bottom: 10px;
+    }
+  `,
+
+  title: css`
+    margin-bottom: 20px;
+    font-size: 26px;
+    font-weight: bold;
+    text-align: center;
+    ${mq.sm} {
+      margin-bottom: 10px;
+      font-size: 22px;
+    }
+  `,
+
+  desc: css`
+    font-size: 16px;
+    ul {
+      margin-left: 1.5em;
+    }
+  `
+}
