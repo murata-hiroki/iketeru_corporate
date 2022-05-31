761d912e5a79ad40bd7ac603d3da84febe6c67a4
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-31 22:25:52 +0900

    Footer emotion移行

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,22 +1,23 @@
-import * as styles from './styles.module.scss'
+import { css } from '@emotion/react'
 import Container from '../Container'
 import Footnote from '../Footnote'
 import logo from '../../images/logo.svg'
+import { colors, mq } from '../../utilities/styleUtilities'
 
 const Footer = () => {
   return (
     <footer id='footer'>
-      <div className={styles.upper}>
+      <div css={styles.upper}>
         <Container>
-          <div className={styles.upperInner}>
+          <div css={styles.upperInner}>
             <Footnote />
-            <p className={styles.copyright}>
+            <p css={styles.copyright}>
               © 2022 イケテルシステム
             </p>
           </div>
         </Container>
       </div>
-      <div className={styles.lower}>
+      <div css={styles.lower}>
         <img src={logo} alt='イケテルシステム' />
       </div>
     </footer>
@@ -24,3 +25,32 @@ const Footer = () => {
 }
 
 export default Footer
+
+const styles = {
+  upper: css`
+    padding: 40px 0;
+    background-color: ${colors.black};
+    color: ${colors.white};
+  `,
+
+  upperInner: css`
+    ${mq.smUp} {
+      display: flex;
+      align-items: flex-end;
+      justify-content: space-between;
+    }
+  `,
+
+  copyright: css`
+    font-size: 14px;
+    ${mq.sm} {
+      margin-top: 20px;
+      text-align: center;
+    }
+  `,
+
+  lower: css`
+    padding: 30px 20px;
+    text-align: center;
+  `
+}
