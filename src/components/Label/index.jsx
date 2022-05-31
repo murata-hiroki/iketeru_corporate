acaba08b299c429747332655c06e6b0544629e82
Yuto Ono <stellarwindmaster@gmail.com>
2022-06-01 08:04:58 +0900

    Label emotion移行

--- src/components/Label/index.jsx
+++ src/components/Label/index.jsx
@@ -1,4 +1,5 @@
-import * as styles from './styles.module.scss'
+import { css } from '@emotion/react'
+import { colors, mq } from '../../utilities/styleUtilities'
 
 const Label = ({
   htmlFor,
@@ -7,12 +8,46 @@ const Label = ({
   comment = null
 }) => {
   return (
-    <label htmlFor={htmlFor} className={styles.label}>
+    <label htmlFor={htmlFor} css={styles.label}>
       {labelText}
-      {required && <span className={styles.required}>必須</span>}
-      {comment && <span className={styles.comment}>{comment}</span>}
+      {required && <span css={styles.required}>必須</span>}
+      {comment && <span css={styles.comment}>{comment}</span>}
     </label>
   )
 }
 
 export default Label
+
+const styles = {
+  label: css`
+    display: block;
+    margin-bottom: 10px;
+    font-size: 22px;
+    font-weight: bold;
+    ${mq.sm} {
+      font-size: 18px;
+    }
+  `,
+
+  required: css`
+    display: inline-block;
+    margin-left: 20px;
+    padding: 2px 20px;
+    border-radius: 4px;
+    background-color: ${colors.red};
+    color: ${colors.white};
+    font-size: 18px;
+    ${mq.sm} {
+      font-size: 16px;
+    }
+  `,
+
+  comment: css`
+    margin-left: 5px;
+    font-size: 18px;
+    font-weight: normal;
+    ${mq.sm} {
+      font-size: 14px;
+    }
+  `
+}
