030137799e2bf20191419a16e53eaf74de46b4ed
Yuto Ono <stellarwindmaster@gmail.com>
2022-06-01 07:53:20 +0900

    Hero emotion移行

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,21 +1,22 @@
-import * as styles from './styles.module.scss'
+import { css } from '@emotion/react'
+import { colors, mq, SP_PADDING } from '../../utilities/styleUtilities'
 import HeroBG from '../HeroBG'
 import ScrollButton from '../ScrollButton'
 
 const Hero = () => {
   return (
-    <div className={styles.outer}>
+    <div css={styles.outer}>
       <HeroBG />
-      <div className={styles.inner}>
-        <h1 className={styles.title}>
+      <div css={styles.inner}>
+        <h1 css={styles.title}>
           まだWordPressを<br />ご利用の方、必見！！
         </h1>
-        <p className={styles.catchcopy}>
+        <p css={styles.catchcopy}>
           あなたのサイト、<br />
           もっと高速に、<br />
           もっとセキュリティを強固に。
         </p>
-        <div className={styles.btnBox}>
+        <div css={styles.btnBox}>
           <ScrollButton target='estimate'>無料お見積もりはこちら</ScrollButton>
         </div>
       </div>
@@ -24,3 +25,48 @@ const Hero = () => {
 }
 
 export default Hero
+
+const styles = {
+  outer: css`
+    position: relative;
+    height: 718px;
+    background-color: ${colors.purple3};
+    ${mq.sm} {
+      height: 430px;
+    }
+  `,
+
+  inner: css`
+    position: relative;
+    padding-top: 120px;
+    ${mq.sm} {
+      padding-top: 60px;
+    }
+  `,
+
+  title: css`
+    font-size: 64px;
+    font-weight: bold;
+    line-height: 1.5;
+    text-align: center;
+    ${mq.sm} {
+      font-size: 32px;
+    }
+  `,
+
+  catchcopy: css`
+    margin-top: 24px;
+    font-size: 30px;
+    line-height: 1.6;
+    text-align: center;
+    ${mq.sm} {
+      font-size: 20px;
+    }
+  `,
+
+  btnBox: css`
+    padding: 0 ${SP_PADDING}px;
+    margin-top: 25px;
+    text-align: center;
+  `
+}
