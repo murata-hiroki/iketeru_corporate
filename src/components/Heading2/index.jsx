6f1ab88d3fc10f8db93dd242b4d94bba71a954a9
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-31 22:35:48 +0900

    Heading2 emotion移行

--- src/components/Heading2/index.jsx
+++ src/components/Heading2/index.jsx
@@ -1,7 +1,30 @@
-import * as styles from './styles.module.scss'
+import { css } from '@emotion/react'
+import { colors, mq } from '../../utilities/styleUtilities'
 
 const Heading2 = ({ children }) => {
-  return <h2 className={styles.h2}>{children}</h2>
+  return <h2 css={styles.h2}>{children}</h2>
 }
 
 export default Heading2
+
+const styles = {
+  h2: css`
+    font-size: 48px;
+    font-weight: bold;
+    line-height: 1.25;
+    text-align: center;
+    margin-bottom: 56px;
+    ${mq.sm} {
+      margin-bottom: 30px;
+      font-size: 32px;
+    }
+    &::after {
+      content: "";
+      display: block;
+      width: 100px;
+      height: 10px;
+      background-color: ${colors.purple};
+      margin: 10px auto 0;
+    }
+  `
+}
