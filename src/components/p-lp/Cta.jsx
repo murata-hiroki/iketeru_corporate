d20e813a95bf706fb68ff63ad653c4f31b06efb5
Yuto Ono <stellarwindmaster@gmail.com>
2022-06-03 21:38:19 +0900

    キャンペーンバナーを一旦削除

--- src/components/p-lp/Cta.jsx
+++ src/components/p-lp/Cta.jsx
@@ -1,30 +1,15 @@
-import { StaticImage } from 'gatsby-plugin-image'
 import { css } from '@emotion/react'
 import ScrollButton from '../common/ScrollButton'
 import { mq } from '../../styles/utils'
 
 const Cta = ({ hideBtn = false }) => {
+  if (hideBtn) {
+    return null
+  }
+
   return (
-    <div>
-      <div css={styles.pcBanner}>
-        <StaticImage
-          src='../../images/banner.png'
-          alt='創業キャンペーン実施中！ 通常価格¥100,000〜 → 先着3件限定 ¥30,000〜！'
-          width={1000}
-          height={437}
-        />
-      </div>
-      <div css={styles.spBanner}>
-        <StaticImage
-          src='../../images/banner-sp.png'
-          alt='創業キャンペーン実施中！ 通常価格¥100,000〜 → 先着3件限定 ¥30,000〜！'
-        />
-      </div>
-      {hideBtn || (
-        <div css={styles.btnBox}>
-          <ScrollButton target='estimate'>無料お見積もりはこちら</ScrollButton>
-        </div>
-      )}
+    <div css={styles.btnBox}>
+      <ScrollButton target='estimate'>無料お見積もりはこちら</ScrollButton>
     </div>
   )
 }
@@ -32,19 +17,6 @@ const Cta = ({ hideBtn = false }) => {
 export default Cta
 
 const styles = {
-  pcBanner: css`
-    ${mq.sm} {
-      display: none;
-    }
-  `,
-
-  spBanner: css`
-    text-align: center;
-    ${mq.smUp} {
-      display: none;
-    }
-  `,
-
   btnBox: css`
     margin-top: 50px;
     text-align: center;
